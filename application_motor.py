8cb44f32bd597caec4b0a51252be5838ec954fd9
Artem Voronkin <a.voronkin@wysegames.com>
2015-01-29 16:47:31 +0200

    refactor

--- application_motor.py
+++ application_motor.py
@@ -8,14 +8,6 @@ import settings
 import os.path
 import motor
 
-try:
-    from settings import DB_HOST, DB_PORT
-    DEFAULT_HOST = DB_HOST
-    DEFAULT_PORT = DB_PORT
-except ImportError:
-    DEFAULT_HOST = 'localhost'
-    DEFAULT_PORT = 27017
-
 
 class Application(tornado.web.Application):
     def __init__(self):
@@ -27,7 +19,7 @@ class Application(tornado.web.Application):
             static_path=os.path.join(os.path.dirname(__file__), "static"),
             xsrf_cookies=True,
             debug=settings.DEBUG,
-            db=motor.MotorClient(DEFAULT_HOST, DEFAULT_PORT)
+            db=motor.MotorClient(settings.DB_HOST, settings.DB_PORT)
         )
         tornado.web.Application.__init__(self, handlers, **options)
 
